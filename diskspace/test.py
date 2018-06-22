d321d17dbbf95b02592113e0cc87330a671d4742
William Almeida <will.allmeida@gmail.com>
2018-06-22 20:07:42 -0300

    Adding print_tree test

--- diskspace/test.py
+++ diskspace/test.py
@@ -1,8 +1,9 @@
-from diskspace import bytes_to_readable, subprocess_check_output, print_tree
+from diskspace import bytes_to_readable, subprocess_check_output, show_space_list, args, print_tree
 import os
 import unittest
-from io import StringIO
+import StringIO
 import sys
+
 
 class TestMethods(unittest.TestCase):
 
@@ -12,7 +13,7 @@ class TestMethods(unittest.TestCase):
         self.cmd = 'du '
         self.path = os.path.abspath('.')
         self.cmd += self.path
-        
+        self.file_tree = {self.path: {'print_size': '50.00Kb', 'children': [], 'size': 3}}
 
     def test_bytes_to_readable(self):
         blocks = 100
@@ -26,6 +27,20 @@ class TestMethods(unittest.TestCase):
         path = subprocess_check_output(self.cmd)
         self.assertIn(self.path, path)
 
+    def test_print_tree(self):
+            capturedOutput = StringIO.StringIO()
+            sys.stdout = capturedOutput
+            print_tree(self.file_tree, self.file_tree[self.path], self.path,
+                       self.largest_size, self.total_size)
+            sys.stdout = sys.__stdout__
+            self.assertEqual('50.00Kb   75%  '+self.path, capturedOutput.getvalue().strip())
+
+    def test_show_space_list(self):
+        capturedOutput = StringIO.StringIO()
+        sys.stdout = capturedOutput
+        show_space_list(args.directory, args.depth, order=(args.order == 'desc'))
+        sys.stdout = sys.__stdout__
+        self.assertIn('Size (%) File' and self.path, capturedOutput.getvalue().strip())
 
 
 
