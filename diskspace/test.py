7e7eeaee728726f7f262567433972da4d5d5fed0
William Almeida <will.allmeida@gmail.com>
2018-06-22 19:39:15 -0300

    Fixing tests

--- diskspace/test.py
+++ diskspace/test.py
@@ -1,16 +1,25 @@
-import unittest
 from diskspace import bytes_to_readable, subprocess_check_output
-import subprocess
+import os
+import unittest
 
 class TestMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.largest_size = 8
+        self.total_size = 4
+        self.cmd = 'du '
+        self.abs_directory = os.path.abspath('.')
+        self.cmd += self.abs_directory
+        self.file_tree = {self.abs_directory: {'print_size': '50.00Kb', 'children': [], 'size': 8}}
 
     def test_bytes_to_readable(self):
         blocks = 100
         self.assertEqual(bytes_to_readable(blocks), "50.00Kb")
 
     def test_subprocess_check_output(self):
-        path = "/test/"
-        self.assertEqual(subprocess_check_output(path),['/test'])
+        path = subprocess_check_output(self.cmd)
+        self.assertIn(self.abs_directory, path)
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
